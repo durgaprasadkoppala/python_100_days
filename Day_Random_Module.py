2a93cadc32eeb1b07070a34ee62d01e5d1b5522a
durgaprasadkoppala <durgarajesh.prasad.10@gmail.com>
2024-05-28 20:32:39 -0500

    Create Day_Random_Module.py

---
+++ Day_Random_Module.py
@@ -0,0 +1,11 @@
+import random
+import my_module
+
+random_number = random.randint(1,10)
+print(random_number)
+
+random_float = random.random()
+print(random_float)
+print(random_float*5)
+
+print(my_module.pi)
